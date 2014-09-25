d2fd2208eefa9838ed8b617da769be0067fef33a
Pascal Betz <pascal.betz@simplificator.com>
2014-09-25 10:59:16 +0200

    formatting

--- lib/eventhub/configuration.rb
+++ lib/eventhub/configuration.rb
@@ -1,27 +1,27 @@
 module EventHub
 
-	class Configuration
-		include Singleton
+  class Configuration
+    include Singleton
     include Helper
 
-		attr_accessor :data, :folder, :environment
-  	
-  	def initialize
-    	@data = nil
+    attr_accessor :data, :folder, :environment
+
+    def initialize
+      @data = nil
       @folder = Dir.pwd
       @environment = 'development'
-  	end
+    end
 
-  	def load_file(input, env='development')
-    	tmp = JSON.parse( IO.read(input))
-  		@data = tmp[env]
+    def load_file(input, env = 'development')
+      tmp = JSON.parse( IO.read(input))
+      @data = tmp[env]
       @environment = env
       true
     rescue => e
       EventHub.logger.info("Unexpected exception while loading configuration [#{input}]: #{format_string(e.message)}")
-  	  false
+      false
     end
 
-	end
+  end
 
 end
